bc2ac3daab7e593880c116c4d8590dedb426b7ef
timl4967 <timothyalehmann@gmail.com>
2016-01-27 23:18:37 -0500

    We added and initialized new variables all beginning with "m_" in accordance with the naming convention posted by Mr. O. We also added some tank drive code but were not able to succesfully deploy it over WiFi, USB, or Ethernet, and therefore it is untested. Lastly we made some diagrams outlining what buttons will be mapped for what controls. The diagrams are currently very crude but will hopefully be improved upon in the future.

--- 2016 Robot Code/src/NIVision.cpp
+++ 2016 Robot Code/src/NIVision.cpp
@@ -1,22 +1,22 @@
-/*
- * NIVision.cpp
- *
- *  Created on: Jan 26, 2016
- *      Author: James
- */
-
-#include <NIVision.h>
-
-NIVision::NIVision() {
-	// TODO Auto-generated constructor stub
-
-}
-
-NIVision::~NIVision() {
-	// TODO Auto-generated destructor stub
-}
-
-void NIVision::init(){
-
-
-}
+///*
+// * NIVision.cpp
+// *
+// *  Created on: Jan 26, 2016
+// *      Author: James
+// */
+//
+//#include <NIVision.h>
+//
+//NIVision::NIVision() {
+//	// TODO Auto-generated constructor stub
+//
+//}
+//
+//NIVision::~NIVision() {
+//	// TODO Auto-generated destructor stub
+//}
+//
+//void NIVision::init(){
+//
+//
+//}
--- 2016 Robot Code/src/Robot.cpp
+++ 2016 Robot Code/src/Robot.cpp
@@ -10,12 +10,12 @@ class Robot: public IterativeRobot
 {
 
 	RobotDrive m_tank;	//normal drive wheels tank drive
-	CANTalon m_turretLazy, m_turretLift, m_turretShoot1, m_turretShoot2;
+	//CANTalon m_turretLazy, m_turretLift, m_turretShoot1, m_turretShoot2;
 	Joystick m_driveStickL, m_driveStickR, m_turretStick; //control joysticks
-	Solenoid m_driveFR, m_driveFL, m_driveRR, m_driveRL; //solenoids that control drive wheel height
-	PowerDistributionPanel PDBoard;
-	Encoder m_lazyCode, m_liftCode, m_shoot1Code, m_shoot2Code;
-	DigitalInput m_homeLiftSwitch, m_lazySwitchF, m_lazySwitchR, m_backLiftSwitch;
+	//Solenoid m_driveFR, m_driveFL, m_driveRR, m_driveRL; //solenoids that control drive wheel height
+	//PowerDistributionPanel PDBoard;
+	//Encoder m_lazyCode, m_liftCode, m_shoot1Code, m_shoot2Code;
+	//DigitalInput m_homeLiftSwitch, m_lazySwitchF, m_lazySwitchR, m_backLiftSwitch;
 
 private:
 
@@ -32,28 +32,27 @@ public:
 
 	Robot():
 		m_tank(0, 4, 2, 6),
-		m_turretLazy(1),
-		m_turretLift(2),
-		m_turretShoot1(3),
-		m_turretShoot2(4),
+//		m_turretLazy(1),
+//		m_turretLift(2),
+//		m_turretShoot1(3),
+//		m_turretShoot2(4),
 		m_driveStickL(1),
 		m_driveStickR(2),
-		m_turretStick(3),
-		m_driveFR(0),
-		m_driveFL(0),
-		m_driveRR(0),
-		m_driveRL(0),
-		PDBoard(0),
-		m_lazyCode(0),
-		m_liftCode(0),
-		m_shoot1Code(0),
-		m_shoot2Code(0),
-		m_homeLiftSwitch(0),
-		m_lazySwitchF(0),
-		m_lazySwitchR(0),
-		m_backLiftSwitch(0)
+		m_turretStick(3)
+//		m_driveFR(0),
+//		m_driveFL(0),
+//		m_driveRR(0),
+//		m_driveRL(0),
+//		PDBoard(0),
+//		m_lazyCode(0),
+//		m_liftCode(0),
+//		m_shoot1Code(0),
+//		m_shoot2Code(0),
+//		m_homeLiftSwitch(0),
+//		m_lazySwitchF(0),
+//		m_lazySwitchR(0)
+//		m_backLiftSwitch(0)
 
-booblyboo
 
 {}
 
@@ -107,6 +106,8 @@ booblyboo
 	void TeleopPeriodic()
 	{
 		ui.GetData(&wui);
+
+		m_tank.TankDrive(m_driveStickL, m_driveStickR);
 	}
 
 	void TestPeriodic()
