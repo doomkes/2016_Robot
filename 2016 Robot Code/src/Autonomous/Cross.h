37704fffc33f81f98359b431afa4a09fb9cdd025
Joey Day <JoeyDay172@gmail.com>
2016-02-07 23:34:45 -0500

    Added basic framework for crossing defences

---
+++ 2016 Robot Code/src/Autonomous/Cross.h
@@ -0,0 +1,34 @@
+/*
+ * Cross.h
+ *
+ *  Created on: Feb 7, 2016
+ *      Author: Joey172
+ */
+
+#ifndef SRC_AUTONOMOUS_CROSSERS_CROSS_H_
+#define SRC_AUTONOMOUS_CROSSERS_CROSS_H_
+
+enum Defense
+{
+	// Catagory A:
+	PORTCULLIS,
+	ROCK_WALL,
+	// Catagory B:
+	RAMPARTS,
+	MOTE,
+	//Catagory C:
+	SALLY_PORT,
+	DRAW_BRIDGE,
+	// Catagory D:
+	ROUGH_TERRAIN,
+	CHEVAL_DE_FRISE,
+	LOW_BAR,
+	SHOVEL_THE_FRIES = CHEVAL_DE_FRISE,
+};
+
+class TankDrive;
+class SuspensionDrive;
+
+bool Cross(Defense, TankDrive*, SuspensionDrive*);
+
+#endif /* SRC_AUTONOMOUS_CROSSERS_CROSS_H_ */
