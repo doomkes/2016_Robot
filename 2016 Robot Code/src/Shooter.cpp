a20ae00df1cb6d0cf483deada851d9e348abdc24
Win7 <Win7@192.168.1.187>
2016-03-04 16:54:28 -0500

    ported visual basic code to cpp, and added it in

--- 2016 Robot Code/src/Shooter.cpp
+++ 2016 Robot Code/src/Shooter.cpp
@@ -7,6 +7,49 @@
 
 #include <Shooter.h>
 #include "robotmap.h"
+/*
+ *  On each iteration, you feed this module
+ *  1) the time change since the last update
+ *  2) the difference between your current position and where you want to go (error)
+ *  3) your last computed velocity
+ *  4) your desired acceleration (used for both acclearting and decelerating)
+ *  5) your max velocity
+ *  this module handles positive and negative errors and velocities
+ *  so if you have a negative velocity, you actually slow down by "increasing"
+ *  to your velocity.  So proper signs are critical in the code below.
+ *  note all of the sign flips between the "target position is in pos direction" and
+ *  the "target position is in neg direction"
+ */
+float Velocity(float delta_t, float error, float current_v, float accel, float max_v) {
+	float time2stop = 0;
+	float dist2stop = 0;
+	float Velocity = 0;
+	if(accel == 0) { //divide by zero protection.
+		return 0.0000001;
+	}
+	time2stop = (fabs(current_v) + accel * delta_t) / accel;
+	dist2stop = 0.5*accel*time2stop*time2stop;
+	if(error < 0) { // target position is in pos direction.
+		if(current_v < 0) { //going wrong way.
+			Velocity = current_v+delta_t*accel;
+		} else if(dist2stop >= fabs(error)) {
+			Velocity = current_v - delta_t*accel;
+		} else if(current_v + delta_t * accel > max_v ) {
+			Velocity = max_v;
+		} else {
+			Velocity = current_v + delta_t * accel;
+		}
+	} else if(current_v > 0) {  //Error > 0.
+		Velocity = current_v - delta_t * accel;
+	} else if(dist2stop >= fabs(error)) {
+		Velocity = current_v + delta_t * accel;
+	} else if (current_v - delta_t * accel < -max_v) {
+		Velocity = -max_v;
+	} else {
+		Velocity = current_v - delta_t * accel;
+	}
+	return Velocity;
+}
 
 Shooter::Shooter() :
 		m_lift(LIFT),
@@ -83,15 +126,25 @@ void Shooter::LiftTo(float angle) {
 }
 
 void Shooter::Update() {
+	static Timer timer;
+
 	m_lift.SetPID(SmartDashboard::GetNumber("Shooter P", 0.0) ,
 				  SmartDashboard::GetNumber("Shooter I", 0.0),
 				  SmartDashboard::GetNumber("Shooter D", 0.0));
 	SmartDashboard::PutNumber("position", m_lift.Get());
 	SmartDashboard::PutNumber("target position", target_position);
-	SmartDashboard::PutNumber("Time", m_timer.Get());
-	m_lift.Set(-m_liftMove.Position(m_timer.Get()));
-	SmartDashboard::PutNumber("trapezoidal Position", m_liftMove.Position(m_timer.Get()));
+	//m_lift.Set(m_liftMove.Position(m_timer.Get()));
+	m_lift.Set(m_lift.Get() +
+			Velocity(timer.Get(),
+					m_lift.Get() - target_position,
+					m_lift.GetSpeed(),
+					SmartDashboard::GetNumber("lift accel", 0),
+					SmartDashboard::GetNumber("lift max speed", 0)));
+
+	timer.Reset();
+	timer.Start();
 }
+
 
 void Shooter::Stop() {
 	m_shoot1.Set(0);
